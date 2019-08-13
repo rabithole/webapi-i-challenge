fc5e7c2716627f8825617221276bcabf9d00bbda
Robert Kulp <pedalinon@gmail.com>
2019-08-12 18:29:08 -0700

    MVP

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ server.get('/api/users', (req, res) => { // This is a route handler...
   // Hubs.find() returns a promise. 
   users.find() // Accesses the hubs data from the database...
     .then(users => {
-      console.log('Users', users);
+      // console.log('Users', users);
       // .json will covert the data passed to json...
       // Also tells the client we're sending json through an HTTP header
       res.status(200).json(users);
@@ -27,47 +27,75 @@ server.get('/api/users', (req, res) => { // This is a route handler...
   })
 });
 
-server.get('/hobbits', (req, res) => {
-  const hobbits = [
-    {
-      id: 1,
-      name: 'Samwise Gamgee',
-    },
-    {
-      id: 2,
-      name: 'Frodo Baggins',
-    },
-  ];
-
-  res.status(200).json(hobbits);
+server.get('/api/users/:id', (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: 'The user information could not be retrieved.' });
+    });
 });
 
 // POST /
 server.post('/api/users', (req, res) => {
-  const newUser = req.body; // Object sent from front end aka client. Postman is used for testing as the client. 
-  console.log('new user', newUser);
+  const { name, bio } = req.body; // Object sent from front end aka client. Postman is used for testing as the client. 
+  // console.log('new user', newUser);
   // Can add validating here. 
-  users.add(newUser)
-    .then(users => {
-      if(user.name && user.bio) {
-              console.log('User Id', users.bio)
-
-        res.status(201).json(users);
-      } else {
-        console.log('else')
-        res.status(404).json({
-          message: 'Invalid user info'
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    users.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            'There was an error while saving the user to the database',
         });
-      }
+      });
+  }
+});
 
-      // res.status(201).json(users);
-  })
-  .catch(error => {
-    res.status(500).json({
-      error: error,
-      message: 'Idiot, you failed to create new user'
-    });
-  });
+// PUT Operaion...
+
+server.put('/api/users/:id', (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    users.update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res
+            .status(404)
+            .json({
+              message: 'The user with the specified ID does not exist.',
+            });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: 'The user information could not be modified.',
+        });
+      });
+  }
 });
 
 // DELETE /hubs/:id
